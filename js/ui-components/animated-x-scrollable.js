f5e4967d33493f8e8dca0ba1a115537cb4943c62
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-08-13 21:54:17 +0800

    feat: AnimatedXScrollable takes DOM string as arg.

--- js/ui-components/animated-x-scrollable.js
+++ js/ui-components/animated-x-scrollable.js
@@ -1,29 +1,9 @@
 class AnimatedXScrollable {
-    build = function (selector) {
+    build = function (selector, content) {
         $(selector).html(
             `<div class="row x-scrollable">
                 
-                <div class="col-abs-width col-default-padding">
-                    <div class="container">
-                        <h1>Item 1</h1>
-                        <p>Item 1</p>
-                        <p>Item 1</p>
-                    </div>
-                </div>
-                
-                <div class="col-abs-width col-default-padding">
-                    <div class="container">
-                        <h1>Item 2</h1>
-                        <p>Item 2</p>
-                    </div>
-                </div>
-                
-                <div class="col-abs-width col-default-padding">
-                    <div class="container">
-                        <h1>Item 3</h1>
-                        <p>Item 3</p>
-                    </div>
-                </div>
+                ${content}
             
             </div>`
         );
