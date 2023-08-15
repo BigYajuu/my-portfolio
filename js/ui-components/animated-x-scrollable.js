4b50f4c36ba684d09ba5f92b6d5eb88712e65a33
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-08-15 09:30:47 +0800

    feat: Added Chevron spaces to XScrollable
    - Chevron space stays in place when div is being scrolled and it should also have correct height.

--- js/ui-components/animated-x-scrollable.js
+++ js/ui-components/animated-x-scrollable.js
@@ -1,11 +1,44 @@
 class AnimatedXScrollable {
+
     build = function (selector, content) {
-        $(selector).html(
-            `<div class="row x-scrollable">
-                
-                ${content}
-            
-            </div>`
-        );
+        var height;
+        var self = this;
+        $(document).ready(function() {
+            // 1) Make scrollable div with populated content
+            $(selector).html(
+                `
+                <div class="row x-scrollable">
+                    ${content}
+                </div>
+                `
+            );
+            // 2) Get height of scrollable div
+            height = $(selector).outerHeight();
+            // 3) Recreate scrollable div and chevrons w/ corrent heights
+            $(selector).html(
+                `
+                <div class="row x-scrollable">
+                    ${self._buildScrollChevronLeft(height)}
+                    ${self._buildScrollChevronRight(height)}
+                    ${content}
+                    
+                </div>
+                `
+            );
+        });
+    }
+
+    _buildScrollChevronLeft = function (height) {
+        return `
+                <div class="chevron-style-left" style="height: ${height}px">
+                </div>
+                `;
+    }
+
+    _buildScrollChevronRight = function (height) {
+        return `
+                <div class="chevron-style-right" style="height: ${height}px">
+                </div>
+                `;
     }
 }
