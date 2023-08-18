5dfe7f839e2e21fc810b607834b379adad37e6ec
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-08-18 17:38:15 +0800

    feat: Right ScrollChevron with animated scroll

--- js/ui-components/animated-x-scrollable.js
+++ js/ui-components/animated-x-scrollable.js
@@ -1,11 +1,18 @@
 class AnimatedXScrollable {
 
+    constructor() {
+        this.scrollPosition = 0;
+        this.scrollSpeed = 0;
+        this.movementIntervalHandler;
+        this.accelerationIntervalHandler;
+    }
+
     build = function (selector, content) {
         var height;
         var self = this;
         $(document).ready(function() {
             // 1) Make scrollable div with populated content
-            $(selector).html(
+            $(`#${selector}`).html(
                 `
                 <div class="row x-scrollable">
                     ${content}
@@ -13,31 +20,59 @@ class AnimatedXScrollable {
                 `
             );
             // 2) Get height of scrollable div
-            height = $(selector).outerHeight();
+            height = $(`#${selector}`).outerHeight();
             // 3) Recreate scrollable div and chevrons w/ corrent heights
-            $(selector).html(
+            var scrollableID = `${selector}-scrollable`
+            var scrollChevronLeftID = `${selector}-scroll-chevron-left`
+            var scrollChevronRightID = `${selector}-scroll-chevron-right`
+            $(`#${selector}`).html(
                 `
-                <div class="row x-scrollable">
-                    ${self._buildScrollChevronLeft(height)}
-                    ${self._buildScrollChevronRight(height)}
+                <div class="row x-scrollable" id="${scrollableID}">
+                    ${self._buildScrollChevronLeft(height, scrollChevronLeftID)}
+                    ${self._buildScrollChevronRight(height, scrollChevronRightID)}
                     ${content}
-                    
                 </div>
                 `
             );
+            // Set scroll listener
+            $(`#${scrollChevronRightID}`).on('mouseenter', function() {
+                // self.scrollSpeed = 0;
+                self.movementIntervalHandler = setInterval(function() {
+                    if (self.scrollSpeed < 10) {
+                        self.scrollSpeed += 1;
+                    }
+                    // console.log(self.scrollSpeed);
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
+                    $(`#${scrollableID}`).scrollLeft(self.scrollPosition);       
+                }, 20);
+            }).on('mouseleave', function(){
+                clearInterval(self.movementIntervalHandler);
+                self.accelerationIntervalHandler = setInterval(function() {
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
+                    $(`#${scrollableID}`).scrollLeft(self.scrollPosition);
+
+                    if (self.scrollSpeed <= 0) {
+                        clearInterval(self.accelerationIntervalHandler);
+                    } else {
+                        self.scrollSpeed -= 1;
+                    }
+                }, 20)
+            });
         });
     }
 
-    _buildScrollChevronLeft = function (height) {
+    _buildScrollChevronLeft = function (height, id) {
         return `
-                <div class="scroll-chevron-left" style="height: ${height}px">
+                <div class="scroll-chevron-left" id="${id}" style="height: ${height}px">
                 </div>
                 `;
     }
 
-    _buildScrollChevronRight = function (height) {
+    _buildScrollChevronRight = function (height, id) {
+
+        
         return `
-                <div class="scroll-chevron-right" style="height: ${height}px">
+                <div class="scroll-chevron-right" id="${id}" style="height: ${height}px">
                 </div>
                 `;
     }
