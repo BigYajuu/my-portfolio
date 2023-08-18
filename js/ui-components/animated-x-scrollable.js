673b275de8726bdf341a027258f6c104aea0977c
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-08-18 23:55:16 +0800

    feat: Applied scroll mechanics to both chevrons

--- js/ui-components/animated-x-scrollable.js
+++ js/ui-components/animated-x-scrollable.js
@@ -1,3 +1,8 @@
+const ScrollDirection = {
+    LEFT: 'left',
+    RIGHT: 'right'
+};
+
 class AnimatedXScrollable {
 
     constructor() {
@@ -8,8 +13,8 @@ class AnimatedXScrollable {
     }
 
     build = function (selector, content) {
-        var height;
-        var self = this;
+        let height;
+        let self = this;
         $(document).ready(function() {
             // 1) Make scrollable div with populated content
             $(`#${selector}`).html(
@@ -22,9 +27,9 @@ class AnimatedXScrollable {
             // 2) Get height of scrollable div
             height = $(`#${selector}`).outerHeight();
             // 3) Recreate scrollable div and chevrons w/ corrent heights
-            var scrollableID = `${selector}-scrollable`
-            var scrollChevronLeftID = `${selector}-scroll-chevron-left`
-            var scrollChevronRightID = `${selector}-scroll-chevron-right`
+            let scrollableID = `${selector}-scrollable` // For the scrollable div encompassing the containers
+            let scrollChevronLeftID = `${selector}-scroll-chevron-left` // Chevron on the left
+            let scrollChevronRightID = `${selector}-scroll-chevron-right`   // Chevron on the right
             $(`#${selector}`).html(
                 `
                 <div class="row x-scrollable" id="${scrollableID}">
@@ -34,30 +39,9 @@ class AnimatedXScrollable {
                 </div>
                 `
             );
-            // Set scroll listener
-            $(`#${scrollChevronRightID}`).on('mouseenter', function() {
-                // self.scrollSpeed = 0;
-                self.movementIntervalHandler = setInterval(function() {
-                    if (self.scrollSpeed < 12) {
-                        self.scrollSpeed += 1;
-                    }
-                    // console.log(self.scrollSpeed);
-                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
-                    $(`#${scrollableID}`).scrollLeft(self.scrollPosition);       
-                }, 15);
-            }).on('mouseleave', function(){
-                clearInterval(self.movementIntervalHandler);
-                self.accelerationIntervalHandler = setInterval(function() {
-                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
-                    $(`#${scrollableID}`).scrollLeft(self.scrollPosition);
-
-                    if (self.scrollSpeed <= 0) {
-                        clearInterval(self.accelerationIntervalHandler);
-                    } else {
-                        self.scrollSpeed -= 1;
-                    }
-                }, 15)
-            });
+            // 4) Setup Mouse Scroll Events
+            self._setupScrollMouseEvent(scrollChevronLeftID, scrollableID, ScrollDirection.LEFT);
+            self._setupScrollMouseEvent(scrollChevronRightID, scrollableID, ScrollDirection.RIGHT);
         });
     }
 
@@ -75,5 +59,41 @@ class AnimatedXScrollable {
                 <div class="scroll-chevron-right" id="${id}" style="height: ${height}px">
                 </div>
                 `;
+    }
+
+    _setupScrollMouseEvent = function (scrollChevronID, scrollableID, direction) {
+        let self = this;
+        $(`#${scrollChevronID}`).on('mouseenter', function() {
+            // self.scrollSpeed = 0;
+            self.movementIntervalHandler = setInterval(function() {
+                if (self.scrollSpeed < 12) {
+                    self.scrollSpeed += 1;
+                }
+                // console.log(self.scrollSpeed);
+                if (direction === ScrollDirection.LEFT) {
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() - self.scrollSpeed;
+                } else if (direction === ScrollDirection.RIGHT) {
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
+                }
+                console.log(self.scrollPosition);
+                $(`#${scrollableID}`).scrollLeft(self.scrollPosition);       
+            }, 15);
+        }).on('mouseleave', function(){
+            clearInterval(self.movementIntervalHandler);
+            self.accelerationIntervalHandler = setInterval(function() {
+                if (direction === ScrollDirection.LEFT) {
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() - self.scrollSpeed;
+                } else if (direction === ScrollDirection.RIGHT) {
+                    self.scrollPosition = $(`#${scrollableID}`).scrollLeft() + self.scrollSpeed;
+                }
+                $(`#${scrollableID}`).scrollLeft(self.scrollPosition);
+    
+                if (self.scrollSpeed <= 0) {
+                    clearInterval(self.accelerationIntervalHandler);
+                } else {
+                    self.scrollSpeed -= 1;
+                }
+            }, 15);
+        });
     }
 }
