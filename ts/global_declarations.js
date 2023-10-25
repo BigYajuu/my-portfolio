8ee8df3aea4d8e199e64849f477bf2288fde399f
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-10-25 22:19:36 +0800

    chore: Component instances now in separate file

--- ts/global_declarations.js
+++ ts/global_declarations.js
@@ -1,6 +1,129 @@
 import { Page } from "./engine/page.js";
 import { PageManagement } from "./engine/page-management.js";
 import { CubicXAxisTransition } from "./engine/cubic-x-axis-transition.js";
-export var page1 = new Page("page-1", new CubicXAxisTransition());
-export var page2 = new Page("page-2", new CubicXAxisTransition());
-export var pageManagement = new PageManagement("page-management-container", [page1, page2]);
+import { AnimatedXScrollable } from "./components/animated-x-scrollable.js";
+export const page1 = new Page("page-1", new CubicXAxisTransition());
+export const page2 = new Page("page-2", new CubicXAxisTransition());
+export const pageManagement = new PageManagement("page-management-container", [page1, page2]);
+export const sectionProjectItems = new AnimatedXScrollable(`
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 1</h1>
+            <p>Item 1</p>
+            <p>Item 1</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 3</h1>
+            <p>Item 3</p>
+        </div>
+    </div>`, "section-projects-items", "page-1", pageManagement);
+export const sectionProjectItems2 = new AnimatedXScrollable(`
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 1</h1>
+            <p>Item 1</p>
+            <p>Item 1</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 3</h1>
+            <p>Item 3</p>
+        </div>
+    </div>`, "section-projects-items-2", "page-2", pageManagement);
+export const sectionProjectItems3 = new AnimatedXScrollable(`
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 1</h1>
+            <p>Item 1</p>
+            <p>Item 1</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 2</h1>
+            <p>Item 2</p>
+        </div>
+    </div>
+    
+    <div class="col-abs-width col-default-padding">
+        <div class="container">
+            <h1>Item 3</h1>
+            <p>Item 3</p>
+        </div>
+    </div>`, "section-projects-items-3", "page-2", pageManagement);
