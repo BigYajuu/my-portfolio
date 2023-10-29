4e45fd243c9db06ce0dc2e585a428d9a3eaed6c9
BigYajuu <62134513+BigYajuu@users.noreply.github.com>
2023-10-29 17:23:31 +0800

    chore: Attempt 8: Adding jekyll config file
    ...to include node_modules

--- ts/initialized_items.js
+++ ts/initialized_items.js
@@ -1,13 +1,11 @@
-import $ from "jquery";
-import {Page} from "./engine/page.js";
-import {PageManagement} from "./engine/page-management.js";
-import {CubicXAxisTransition} from "./engine/cubic-x-axis-transition.js";
-import {AnimatedXScrollable} from "./components/animated-x-scrollable.js";
+import { Page } from "./engine/page.js";
+import { PageManagement } from "./engine/page-management.js";
+import { CubicXAxisTransition } from "./engine/cubic-x-axis-transition.js";
+import { AnimatedXScrollable } from "./components/animated-x-scrollable.js";
 const page1 = new Page("page-1", new CubicXAxisTransition());
 const page2 = new Page("page-2", new CubicXAxisTransition());
 const pageManagement = new PageManagement("page-management-container", [page1, page2]);
-const sectionProjectItems = new AnimatedXScrollable(
-    `
+const sectionProjectItems = new AnimatedXScrollable(`
     <div class="col-abs-width col-default-padding">
         <div class="container">
             <h1>Item 1</h1>
@@ -49,14 +47,8 @@ const sectionProjectItems = new AnimatedXScrollable(
             <h1>Item 3</h1>
             <p>Item 3</p>
         </div>
-    </div>`,
-    "section-projects-items", 
-    page1, 
-    pageManagement
-).build();
-
-export const sectionProjectItems2 = new AnimatedXScrollable(
-    `
+    </div>`, "section-projects-items", page1, pageManagement).build();
+export const sectionProjectItems2 = new AnimatedXScrollable(`
     <div class="col-abs-width col-default-padding">
         <div class="container">
             <h1>Item 1</h1>
@@ -77,13 +69,8 @@ export const sectionProjectItems2 = new AnimatedXScrollable(
             <h1>Item 3</h1>
             <p>Item 3</p>
         </div>
-    </div>`,
-    "section-projects-items-2", 
-    page2, 
-    pageManagement
-).build();
-export const sectionProjectItems3 = new AnimatedXScrollable(
-    `
+    </div>`, "section-projects-items-2", page2, pageManagement).build();
+export const sectionProjectItems3 = new AnimatedXScrollable(`
     <div class="col-abs-width col-default-padding">
         <div class="container">
             <h1>Item 1</h1>
@@ -139,11 +126,5 @@ export const sectionProjectItems3 = new AnimatedXScrollable(
             <h1>Item 3</h1>
             <p>Item 3</p>
         </div>
-    </div>`,
-    "section-projects-items-3", 
-    page2, 
-    pageManagement
-).build();
-
+    </div>`, "section-projects-items-3", page2, pageManagement).build();
 console.log("Initialized Items");
-
